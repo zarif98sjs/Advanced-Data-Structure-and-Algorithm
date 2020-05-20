6be53515d3207b00cfb57ef1957a73007a861de8
Md. Zarif Ul Alam <zarif98sjs@gmail.com>
2020-05-20 17:15:33 +0600

    Meet In The Middle Added

--- Algorithm/16 Meet In The Middle.cpp
+++ Algorithm/16 Meet In The Middle.cpp
@@ -1,8 +1,32 @@
 
 /**
 
-Disjoint Set
-============
+Meet In The Middle
+==================
+
+Demo Problem : LightOj 1127 ( Funny Knapsack )
+----------------------------------------------
+
+Given n integers and a knapsack of weight W,
+you have to count the number of combinations for which you can add the items in the knapsack without overflowing the weight.
+
+1 ≤ n ≤ 30 and 1 ≤ W ≤ 2 * 1e9
+
+Idea
+----
+The idea is that you split the large set into two small sets, small enough for you to handle them using the standard algorithm you know,
+and then use the result from these two small sets to arrive at the required solution.
+
+Complexity
+----------
+O(2^(n / 2) log (2^(n / 2))
+
+Related Problems
+----------------
+LightOj 1127
+LightOj 1235
+CodeChef : Nice subsets (INSQ15_C)
+
 
 **/
 
@@ -67,43 +91,24 @@ string to_str(LL x)
 //
 //}
 
-class DisjointSet
+vector<LL> subsetSum(const vector<LL> &v)
 {
-public:
+    int n = v.size();
 
-    unordered_map<int, int> parent;
+    vector<LL>sum;
 
-    void makeSet(int N)
+    for(int i=0;i<(1<<n);i++)
     {
-        for (int i = 1; i <= N; i++)
-            parent[i] = i;
+        LL s = 0;
+        for(int j=0;j<n;j++)
+        {
+            if(i&(1<<j))
+                s += v[j];
+        }
+        sum.push_back(s);
     }
-
-    int Find(int k)
-    {
-        // if k is root
-        if (parent[k] == k)
-            return k;
-
-        return parent[k] = Find(parent[k]);
-    }
-
-    void Union(int a, int b)
-    {
-        int x = Find(a);
-        int y = Find(b);
-
-        parent[y] = x;
-    }
-
-    void printParent(int N)
-    {
-        for (int i = 1; i <= N; i++)
-            cout<<parent[i]<<" ";
-        cout<<endl;
-
-    }
-};
+    return sum;
+}
 
 int main()
 {
@@ -111,22 +116,53 @@ int main()
 
     optimizeIO();
 
-    DisjointSet ds;
+    int tc;
+    cin>>tc;
 
-    ds.makeSet(10);
+    for(int q=1; q<=tc; q++)
+    {
+        int n;
+        LL w;
+        cin>>n>>w;
 
-    ds.Union(1,2);
-    ds.Union(2,5);
-    ds.Union(5,10);
-    ds.Union(3,4);
+        vector<LL>v1,v2;
 
-    for(int i=1;i<=10;i++)
-        cout<<"Component of "<<i<<" : "<<ds.Find(i)<<endl;
+        for(int i=0;i<n;i++)
+        {
+            LL x;
+            cin>>x;
+
+            if(i&1) v2.push_back(x);
+            else v1.push_back(x);
+        }
+
+        vector<LL> subsetSum1 = subsetSum(v1);
+        vector<LL> subsetSum2 = subsetSum(v2);
+
+        sort(ALL(subsetSum2));
+
+        LL ans = 0;
+
+        for(LL s1:subsetSum1)
+        {
+            LL s2 = w - s1;
+            ans += upper_bound(ALL(subsetSum2),s2) - subsetSum2.begin();
+        }
+
+        cout<<"Case "<<q<<": ";
+        cout<<ans<<endl;
+
+    }
+
 
     return 0;
 }
 
 /**
+
+10
+4 10
+1 4 2 8
 
 **/
 
