c2dc31e5aeb5a43cb520feb56692393842a9224d
Md. Zarif Ul Alam <zarif98sjs@gmail.com>
2020-07-02 22:47:36 +0600

    string added

---
+++ Algorithm/44 Prefix Function KMP.cpp
@@ -0,0 +1,145 @@
+
+/**
+
+Algorithm       : Calculating Prefix Function using KMP Algorithm
+Time Complexity : O(N)
+
+
+Definition:
+The prefix function for this string is defined as an array π of length n,
+where pi[i] is the length of the longest proper prefix of the substring s[0…i] which is also a suffix of this substring.
+A proper prefix of a string is a prefix that is not equal to the string itself. By definition, pi[0]=0.
+
+**/
+
+/** Which of the favors of your Lord will you deny ? **/
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define LL long long
+#define PII pair<int,int>
+#define PLL pair<LL,LL>
+#define MP make_pair
+#define F first
+#define S second
+
+#define ALL(x) (x).begin(), (x).end()
+#define DBG(x) cout << __LINE__ << " says: " << #x << " = " << (x) << endl
+#define READ        freopen("alu.txt", "r", stdin)
+#define WRITE       freopen("vorta.txt", "w", stdout)
+
+#include <ext/pb_ds/assoc_container.hpp>
+#include <ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+
+template<class TIn>using indexed_set = tree<TIn, null_type, less<TIn>,rb_tree_tag, tree_order_statistics_node_update>;
+
+/**
+
+PBDS
+-------------------------------------------------
+1) insert(value)
+2) erase(value)
+3) order_of_key(value) // 0 based indexing
+4) *find_by_order(position) // 0 based indexing
+
+**/
+
+template<class T1, class T2>
+ostream &operator <<(ostream &os, pair<T1,T2>&p);
+template <class T>
+ostream &operator <<(ostream &os, vector<T>&v);
+template <class T>
+ostream &operator <<(ostream &os, set<T>&v);
+
+inline void optimizeIO()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+}
+
+const int nmax = 2e5+7;
+const LL LINF = 1e17;
+
+template <class T>
+string to_str(T x)
+{
+    stringstream ss;
+    ss<<x;
+    return ss.str();
+}
+
+//bool cmp(const PII &A,const PII &B)
+//{
+//
+//}
+
+vector<int> prefix_function(string s)
+{
+    int n = (int)s.length();
+    vector<int> pi(n);
+    for (int i = 1; i < n; i++)
+    {
+        int j = pi[i-1];
+        while (j > 0 && s[i] != s[j])
+            j = pi[j-1];
+        if (s[i] == s[j])
+            j++;
+        pi[i] = j;
+    }
+    return pi;
+}
+
+int main()
+{
+    optimizeIO();
+
+    while(1)
+    {
+        string s;
+        cin>>s;
+
+        vector<int>pi = prefix_function(s);
+
+        cout<<pi<<endl;
+    }
+
+    return 0;
+}
+
+/**
+
+**/
+
+template<class T1, class T2>
+ostream &operator <<(ostream &os, pair<T1,T2>&p)
+{
+    os<<"{"<<p.first<<", "<<p.second<<"} ";
+    return os;
+}
+template <class T>
+ostream &operator <<(ostream &os, vector<T>&v)
+{
+    os<<"[ ";
+    for(int i=0; i<v.size(); i++)
+    {
+        os<<v[i]<<" " ;
+    }
+    os<<" ]";
+    return os;
+}
+
+template <class T>
+ostream &operator <<(ostream &os, set<T>&v)
+{
+    os<<"[ ";
+    for(T i:v)
+    {
+        os<<i<<" ";
+    }
+    os<<" ]";
+    return os;
+}
+
+
